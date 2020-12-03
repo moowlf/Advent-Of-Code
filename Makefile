9ff8f8ea00c7f1e70b4089841deb1135058b79f3
moowlf <tiago.lpneto@outlook.com>
2020-12-03 12:45:05 +0000

    :santa: Initial Commit :christmas_tree:

---
+++ Makefile
@@ -0,0 +1,36 @@
+
+# Compiler
+CXX_COMPILER := g++
+
+# Variables that hold information about building 
+OUTPUT_FOLDER 			:= ./output
+OUTPUT_OBJECT_FOLDER 	:= ./output/objects
+EXECUTABLE				:= problem
+CURRENT_FOLDER			:= 2020
+
+# Flags
+CXX_FLAGS 			:= -std=c++20 -Wall -g -O0
+LDFLAGS 			:= -L/usr/lib
+INCLUDE_FOLDER 		:= -I./libs
+
+# Object Generator
+PROBLEM_SRC 	:= $(CURRENT_FOLDER)/main.cpp
+OBJECT_FILES 	:= $(PROBLEM_SRC:$(CURRENT_FOLDER)/%.cpp=$(OUTPUT_OBJECT_FOLDER)/%.o)
+
+# Rules
+$(OUTPUT_OBJECT_FOLDER)/%.o: $(CURRENT_FOLDER)/%.cpp
+	@mkdir -p $(@D)
+	$(CXX_COMPILER) $(CXX_FLAGS) $(INCLUDE_FOLDER) -c $< -o $@ $(LDFLAGS)
+
+$(EXECUTABLE): $(OBJECT_FILES)
+	$(CXX_COMPILER) $(CXX_FLAGS) -o $(OUTPUT_FOLDER)/$(EXECUTABLE) $^ $(LDFLAGS)
+
+all: build $(EXECUTABLE)
+
+build: p
+	@mkdir -p $(OUTPUT_FOLDER)
+	@mkdir -p $(OUTPUT_OBJECT_FOLDER)
+	@touch $(OUTPUT_FOLDER)/.gitkeep
+
+clean:
+	@rm -r $(OUTPUT_FOLDER)
---
+++ create.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+set -e
+
+CookieJar=".cookies"
+CurrentYear="2020"
+
+# Function responsible for enforcing some rules related to the repository.
+check_git_repository()
+{
+    # Check repository current branch
+    CurrentBranch=$(git branch --show-current)
+    if [ $CurrentBranch != "master" ];then
+        echo "Error. Git repository must be in master."
+        exit 1;
+    fi
+
+    # Check if repository has zero differences
+    NumberOfCommitedFiles=$(git diff --numstat | wc -l)
+    if [ $NumberOfCommitedFiles -ne 0 ];then
+        echo "Error. Git repository has uncommited changes."
+        exit 1;
+    fi
+}
+
+get_input_file()
+{
+    currentDay="$1"
+    outputFolder="$2"
+    curl -b "$CookieJar" "https://adventofcode.com/$CurrentYear/day/$currentDay/input" -o "$outputFolder/input.txt"
+}
+
+# Function that creates both the folder and the file of the problem
+create_folder_file_in_new_branch()
+{   
+    problemIdentifier=$1
+
+    # Folder without spaces
+    FolderName=$CurrentYear/$(printf %02d "$problemIdentifier")
+    
+    git checkout -b $FolderName
+
+    # Create folders
+    InputFolder="$FolderName/inputs"
+    mkdir -p $FolderName $InputFolder
+
+    get_input_file "$problemIdentifier" "$InputFolder"
+    
+    # Create file
+    FileContent="
+    \n
+    // Cpp Includes\n
+    \n
+    int main(){\n
+    }\n
+    "
+    echo $FileContent > "$FolderName/main.cpp"
+}
+
+# Function calls
+check_git_repository
+read -p "> What's the problem identifier? " problemIdentifier
+create_folder_file_in_new_branch $problemIdentifier
